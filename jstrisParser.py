5fde9099daedab6c10adf96a3a2b021a8408b573
Tae Kyu Kim <taekyukim02@gmail.com>
2021-12-31 14:40:37 -0800

    add datatype to pandas dataframe

--- jstrisParser.py
+++ jstrisParser.py
@@ -15,19 +15,19 @@ import time
 
 ### CUSTOM DEFINITIONS ###
 
-class KeyMap:	
-	# key mappings
-	ccw = keyboard.KeyCode.from_char("q")
-	cw = keyboard.KeyCode.from_char("w")
-	rot180 = keyboard.KeyCode.from_char("p")
-	hold = keyboard.KeyCode.from_char("e")
-	left = keyboard.Key.left
-	right = keyboard.Key.right
-	softdrop = keyboard.Key.down
-	harddrop = keyboard.Key.up
+class KeyMap:
+    # key mappings
+    ccw = keyboard.KeyCode.from_char("q")
+    cw = keyboard.KeyCode.from_char("w")
+    rot180 = keyboard.KeyCode.from_char("p")
+    hold = keyboard.KeyCode.from_char("e")
+    left = keyboard.Key.left
+    right = keyboard.Key.right
+    softdrop = keyboard.Key.down
+    harddrop = keyboard.Key.up
 
-	# key list
-	key_list = [ccw, cw, rot180, hold, left, right, softdrop, harddrop]
+    # key list
+    key_list = [ccw, cw, rot180, hold, left, right, softdrop, harddrop]
 
 
 # black:	RGB(red=0, green=0, blue=0)
@@ -39,133 +39,136 @@ class KeyMap:
 # green:	RGB(red=88, green=177, blue=36)
 # red:		RGB(red=216, green=15, blue=54)
 def get_color(pix):
-	mapping = {
-		0: "empty",
-		155: "I",
-		159: "O",
-		41: "T",
-		91: "L",
-		65: "J",
-		177: "S",
-		15: "Z",
-	}	
-	return mapping.get(pix[1], None)
+    mapping = {
+        0: "empty",
+        155: "I",
+        159: "O",
+        41: "T",
+        91: "L",
+        65: "J",
+        177: "S",
+        15: "Z",
+    }
+    return mapping.get(pix[1], None)
 
 
 ### GLOBAL VARIABLES ###
 
 # initialize pandas dataframe of moves
 max_moves = 1000
-moves = pd.DataFrame(index=np.arange(0, max_moves), columns=("block1", "block2", "tot_time", "think_time", "place_time", "switch_time", "switched"))
+moves = pd.DataFrame(index=np.arange(0, max_moves),
+                     columns={"block1": pd.Series(dtype='category'),
+                              "block2": pd.Series(dtype='category'),
+                              "tot_time": pd.Series(dtype='float'),
+                              "think_time": pd.Series(dtype='float'),
+                              "place_time": pd.Series(dtype='float'),
+                              "switch_time": pd.Series(dtype='float'),
+                              "switched": pd.Series(dtype='bool')})
 move_index = 0
 
 # timing game moves
-t_game = None		# start of game
-t_move = None		# start of move
-t_switch = None		# when held is pressed (or start of move if not held)
-t_place = None		# start of placement movement
+t_game = None  # start of game
+t_move = None  # start of move
+t_switch = None  # when held is pressed (or start of move if not held)
+t_place = None  # start of placement movement
 
 
 def update(monitor, block_location):
+    sct = mss.mss()
 
-	sct = mss.mss()
+    global t_game, t_move, t_switch
+    t_game = time.time()
+    t_move = time.time()
+    t_switch = t_move
 
-	global t_game, t_move, t_switch
-	t_game = time.time()
-	t_move = time.time()
-	t_switch = t_move
+    # callback functions
+    def on_press(key):
+        global max_moves, moves, move_index, t_game, t_switch, t_move, t_place
 
-	# callback functions
-	def on_press(key):
-		global max_moves, moves, move_index, t_game, t_switch, t_move, t_place
+        # start movement
+        if (key in KeyMap.key_list and t_place == None):
+            t_place = time.time()
 
-		# start movement
-		if (key in KeyMap.key_list and t_place == None):
-			t_place = time.time()
+        # end move
+        if (key == KeyMap.harddrop):
+            t_move_end = time.time()
+            moves.loc[move_index]["tot_time"] = (t_move_end - t_move)
+            moves.loc[move_index]["think_time"] = (t_place - t_move)
+            moves.loc[move_index]["place_time"] = (t_move_end - t_switch)
+            moves.loc[move_index]["switch_time"] = (t_switch - t_move)
 
-		# end move
-		if (key == KeyMap.harddrop):
-			t_move_end = time.time()
-			moves.loc[move_index]["tot_time"] = (t_move_end - t_move)
-			moves.loc[move_index]["think_time"] = (t_place - t_move)
-			moves.loc[move_index]["place_time"] = (t_move_end - t_switch)
-			moves.loc[move_index]["switch_time"] = (t_switch - t_move)
+            # DEBUG: print dataframe row
+            print(moves.loc[move_index])
 
-			# DEBUG: print dataframe row
-			print(moves.loc[move_index])
-				
-			# get new block
-			move_index = move_index + 1
-			img = np.array(sct.grab(monitor))
-			pix = img[block_location[0]*2, block_location[1]*2, ]
-			moves.loc[move_index]["block1"] = get_color(pix)
+            # get new block
+            move_index = move_index + 1
+            img = np.array(sct.grab(monitor))
+            pix = img[block_location[0] * 2, block_location[1] * 2,]
+            moves.loc[move_index]["block1"] = get_color(pix)
+            moves.loc[move_index]["switched"] = False
 
-			# start new move timing
-			t_move = time.time()
-			t_switch = t_move
-			t_place = None
-			return
+            # start new move timing
+            t_move = time.time()
+            t_switch = t_move
+            t_place = None
+            return
 
-		# hold block
-		if(key == KeyMap.hold and move_index > 0):
-			img = np.array(sct.grab(monitor))
-			pix = img[block_location[0]*2, block_location[1]*2, ]
-			moves.loc[move_index]["block2"] = get_color(pix)
-			moves.loc[move_index]["switched"] = True
+        # hold block
+        if (key == KeyMap.hold and move_index > 0):
+            img = np.array(sct.grab(monitor))
+            pix = img[block_location[0] * 2, block_location[1] * 2,]
+            moves.loc[move_index]["block2"] = get_color(pix)
+            moves.loc[move_index]["switched"] = True
 
-			t_switch = time.time()
-			t_place = None
-			return
+            t_switch = time.time()
+            t_place = None
+            return
 
-		if(not key in KeyMap.key_list):
-			print("pressed other key:", key)
-			return
-				
-	# don't do anything when key is released
-	def on_release(key):
-		return
+        if (not key in KeyMap.key_list):
+            print("pressed other key:", key)
+            return
 
-	listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-	listener.start()
+    # don't do anything when key is released
+    def on_release(key):
+        return
 
-	while 1:
-		pass
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
 
-		
+    while 1:
+        pass
 
 
 def main():
+    # find matrix location
+    print("Finding the board...")
+    newgame_location = None
+    while (newgame_location == None):
+        print("Please ensure \"New Game\" button is visible on screen.")
+        newgame_location = py.locateOnScreen('./assets/newgame.png')
 
-	# find matrix location
-	print("Finding the board...")
-	newgame_location = None
-	while(newgame_location == None):
-		print("Please ensure \"New Game\" button is visible on screen.")
-		newgame_location = py.locateOnScreen('./assets/newgame.png')
+    square_length = 24  # width / length of square in matrix
+    matrix_height = square_length * 20
+    matrix_width = square_length * 10
 
-	square_length = 24 		# width / length of square in matrix
-	matrix_height = square_length * 20
-	matrix_width = square_length * 10
+    monitor = {"top": newgame_location.top // 2 - 502, "left": newgame_location.left // 2 - 145, "width": matrix_width,
+               "height": matrix_height}
 
-	monitor = {"top": newgame_location.top//2 - 502, "left": newgame_location.left//2 - 145, "width": matrix_width, "height": matrix_height}
-	
+    # important locations relative to matrix
+    go_location = (124, 290)
+    block_location = (square_length // 2, 9 * square_length // 2)
 
-	# important locations relative to matrix
-	go_location = (124, 290)
-	block_location = (square_length//2, 9*square_length//2)
+    # wait until game starts
+    #	go_color = py.pixel(710, 473)[0]
+    #	print(go_color)
 
-
-	# wait until game starts
-#	go_color = py.pixel(710, 473)[0]
-#	print(go_color)
-
-	# run game
-	try:
-		print("Parsing jstris...")
-		update(monitor, block_location)
-	except KeyboardInterrupt: # If Ctrl + C input
-		exit('Ctrl + C Input - Terminating')
+    # run game
+    try:
+        print("Parsing jstris...")
+        update(monitor, block_location)
+    except KeyboardInterrupt:  # If Ctrl + C input
+        exit('Ctrl + C Input - Terminating')
 
 
 if __name__ == '__main__':
-	main()
+    main()
